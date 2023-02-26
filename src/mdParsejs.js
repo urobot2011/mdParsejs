790695b51c93c03333590e605f632b41995fe7c3
esp32-urobot <urobot2011@gmail.com>
2023-02-26 15:17:50 +0900

    Update mdParsejs.js

--- src/mdParsejs.js
+++ src/mdParsejs.js
@@ -1,12 +1,12 @@
 (function(){
   function parse(md){
-    //md = md.replace(/^\s*\n\*/gm, '<ul>\n*');
-    //md = md.replace(/^(\*.+)\s*\n([^\*])/gm, '$1\n</ul>\n\n$2');
-    //md = md.replace(/^\*(.+)/gm, '<li>$1</li>');
-    //md = md.replace(/^\s*\n\d\./gm, '<ol>\n1.');
-    //md = md.replace(/^(\d\..+)\s*\n([^\d\.])/gm, '$1\n</ol>\n\n$2');
-    //md = md.replace(/^\d\.(.+)/gm, '<li>$1</li>');
-    //md = md.replace(/^\>(.+)/gm, '<blockquote>$1</blockquote>');
+    md = md.replace(/^\s*\n\*/gm, '<ul>\n*');
+    md = md.replace(/^(\*.+)\s*\n([^\*])/gm, '$1\n</ul>\n\n$2');
+    md = md.replace(/^\*(.+)/gm, '<li>$1</li>');
+    md = md.replace(/^\s*\n\d\./gm, '<ol>\n1.');
+    md = md.replace(/^(\d\..+)\s*\n([^\d\.])/gm, '$1\n</ol>\n\n$2');
+    md = md.replace(/^\d\.(.+)/gm, '<li>$1</li>');
+    md = md.replace(/^\>(.+)/gm, '<blockquote>$1</blockquote>');
     md = md.replace(/[\&]{1}(.+)/g, '<p>$1</p>');
     md = md.replace(/[\#]{6}(.+)/g, '<h6>$1</h6>');
     md = md.replace(/[\#]{5}(.+)/g, '<h5>$1</h5>');
@@ -14,9 +14,9 @@
     md = md.replace(/[\#]{3}(.+)/g, '<h3>$1</h3>');
     md = md.replace(/[\#]{2}(.+)/g, '<h2>$1</h2>');
     md = md.replace(/[\#]{1}(.+)/g, '<h1>$1</h1>');
-    md = md.replace(/^(.+)\n\=+/gm, '<h1>$1</h1>');
-    md = md.replace(/^(.+)\n\-+/gm, '<h2>$1</h2>');
-    /*md = md.replace(/\!\[([^\]]+)\]\(([^\)]+)\)/g, '<img src="$2" alt="$1" />');
+    //md = md.replace(/^(.+)\n\=+/gm, '<h1>$1</h1>');
+    //md = md.replace(/^(.+)\n\-+/gm, '<h2>$1</h2>');
+    md = md.replace(/\!\[([^\]]+)\]\(([^\)]+)\)/g, '<img src="$2" alt="$1" />');
     md = md.replace(/[\[]{1}([^\]]+)[\]]{1}[\(]{1}([^\)\"]+)(\"(.+)\")?[\)]{1}/g, '<a href="$2" title="$4">$1</a>');
     md = md.replace(/[\*\_]{2}([^\*\_]+)[\*\_]{2}/g, '<b>$1</b>');
     md = md.replace(/[\*\_]{1}([^\*\_]+)[\*\_]{1}/g, '<i>$1</i>');
@@ -24,7 +24,7 @@
     md = md.replace(/^\s*\n\`\`\`(([^\s]+))?/gm, '<pre code_l="$2">');
     md = md.replace(/^\`\`\`\s*\n/gm, '</pre>\n\n');
     md = md.replace(/[\`]{1}([^\`]+)[\`]{1}/g, '<code>$1</code>');
-    md = md.replace(/(\<pre.+\>)\s*\n\<p\>(.+)\<\/p\>/gm, '$1$2');*/
+    md = md.replace(/(\<pre.+\>)\s*\n\<p\>(.+)\<\/p\>/gm, '$1$2');
 
     return md;
   }
